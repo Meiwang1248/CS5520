cdff8e32f58465fa683b733d1b4816986080c1e1
Meiwang1248 <meiwang1248@hotmail.com>
2021-05-26 14:11:11 -0700

    Testing the W3 branch

--- app/src/main/java/edu/neu/cs5520/numad21su_shuweiwang/MainActivity.java
+++ app/src/main/java/edu/neu/cs5520/numad21su_shuweiwang/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,30 @@ public class MainActivity extends AppCompatActivity {
     public void introduceMyself(View view) {
         TextView textView = findViewById(R.id.myInfo);
         textView.setVisibility(View.VISIBLE);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.A_button:
+                Toast.makeText(getApplicationContext(), "Pressed: A ", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.B_button:
+                Toast.makeText(getApplicationContext(), "Pressed: B ", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.C_button:
+                Toast.makeText(getApplicationContext(), "Pressed: C ", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.D_button:
+                Toast.makeText(getApplicationContext(), "Pressed: D ", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.E_button:
+                Toast.makeText(getApplicationContext(), "Pressed: E ", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.F_button:
+                Toast.makeText(getApplicationContext(), "Pressed: F ", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
     }
 
 
