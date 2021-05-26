fdc6f81016d64bfbab487dea5a7c9851fec2af40
Meiwang1248 <meiwang1248@hotmail.com>
2021-05-26 14:34:18 -0700

    BUTTON PRESSED UPDATED

--- app/src/main/java/edu/neu/cs5520/numad21su_shuweiwang/MainActivity.java
+++ app/src/main/java/edu/neu/cs5520/numad21su_shuweiwang/MainActivity.java
@@ -3,6 +3,7 @@ package edu.neu.cs5520.numad21su_shuweiwang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,23 +24,37 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.A_button:
-                Toast.makeText(getApplicationContext(), "Pressed: A ", Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(getApplicationContext(), "Pressed: A ", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, -350);
+                toast.show();
                 break;
             case R.id.B_button:
-                Toast.makeText(getApplicationContext(), "Pressed: B ", Toast.LENGTH_SHORT).show();
+                toast = Toast.makeText(getApplicationContext(), "Pressed: B ", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, -350);
+                toast.show();
                 break;
             case R.id.C_button:
-                Toast.makeText(getApplicationContext(), "Pressed: C ", Toast.LENGTH_SHORT).show();
+                toast = Toast.makeText(getApplicationContext(), "Pressed: C ", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, -350);
+                toast.show();
                 break;
             case R.id.D_button:
-                Toast.makeText(getApplicationContext(), "Pressed: D ", Toast.LENGTH_SHORT).show();
+                toast = Toast.makeText(getApplicationContext(), "Pressed: D ", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, -350);
+                toast.show();
                 break;
             case R.id.E_button:
-                Toast.makeText(getApplicationContext(), "Pressed: E ", Toast.LENGTH_SHORT).show();
+                toast = Toast.makeText(getApplicationContext(), "Pressed: E ", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, -350);
+                toast.show();
                 break;
             case R.id.F_button:
-                Toast.makeText(getApplicationContext(), "Pressed: F ", Toast.LENGTH_SHORT).show();
-
+                toast = Toast.makeText(getApplicationContext(), "Pressed: F ", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, -350);
+                toast.show();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + view.getId());
         }
     }
 
